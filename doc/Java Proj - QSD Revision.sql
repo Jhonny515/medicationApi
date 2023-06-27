b8640b538fd55e28138b7b4f3decd1bea18b9179
Jhonny515 <jonatascemporcentojesus@gmail.com>
2023-06-27 19:59:09 -0300

    Initial commit

---
+++ doc/Java Proj - QSD Revision.sql
@@ -0,0 +1,52 @@
+CREATE TABLE `tb_medicamento` (
+  `id` bigint PRIMARY KEY,
+  `nome` varchar2,
+  `principio_ativo` varchar2,
+  `descricao` varchar2,
+  `marca` varchar2,
+  `fabricante` varchar2,
+  `preco` double,
+  `preco_desconto` double,
+  `retencao` boolean,
+  `termolabel` boolean
+);
+
+CREATE TABLE `tb_med_sob_prescricao` (
+  `medicamento_id` bigint PRIMARY KEY
+);
+
+CREATE TABLE `tb_med_injetavel` (
+  `medicamento_id` bigint PRIMARY KEY,
+  `tipo_aplicacao` ENUM ('ENDOVENOSA', 'INTRADERMICA', 'INTRAMUSCULAR', 'SUBCUTANEA')
+);
+
+CREATE TABLE `tb_pedido` (
+  `id` bigint PRIMARY KEY,
+  `cliente_id` bigint,
+  `id_status` int COMMENT 'Um status pode estar em vários pedidos. Um pedido tem somente um status.'
+);
+
+CREATE TABLE `tb_status_pedido` (
+  `id` int PRIMARY KEY,
+  `status` varchar2 COMMENT '1: EM CARRINHO;
+2: FINALIZADO;
+-- Novos status podem ser adicionados --'
+);
+
+CREATE TABLE `tb_carrinho` (
+  `id` bigint PRIMARY KEY,
+  `id_pedido` bigint,
+  `id_medicamento` bigint
+);
+
+ALTER TABLE `tb_status_pedido` COMMENT = 'Um status pode estar em vários pedidos. Um pedido tem somente um status.';
+
+ALTER TABLE `tb_med_sob_prescricao` ADD FOREIGN KEY (`medicamento_id`) REFERENCES `tb_medicamento` (`id`);
+
+ALTER TABLE `tb_med_injetavel` ADD FOREIGN KEY (`medicamento_id`) REFERENCES `tb_medicamento` (`id`);
+
+ALTER TABLE `tb_pedido` ADD FOREIGN KEY (`id_status`) REFERENCES `tb_status_pedido` (`id`);
+
+ALTER TABLE `tb_carrinho` ADD FOREIGN KEY (`id_pedido`) REFERENCES `tb_pedido` (`id`);
+
+ALTER TABLE `tb_carrinho` ADD FOREIGN KEY (`id_medicamento`) REFERENCES `tb_medicamento` (`id`);
