3e69174b512567049ed642fa7069cc8cc107e50c
Jhonny515 <jonatascemporcentojesus@gmail.com>
2023-07-12 11:27:59 -0300

    feat: Novos métodos de Pedido implementados no Controller

--- src/main/java/com/jhonny/medicationApi/controllers/PedidoController.java
+++ src/main/java/com/jhonny/medicationApi/controllers/PedidoController.java
@@ -5,8 +5,11 @@ import com.jhonny.medicationApi.dtos.inputs.PedidoSearchInputDTO;
 import com.jhonny.medicationApi.services.PedidoService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,14 +23,24 @@ public class PedidoController {
     @Autowired
     private PedidoService service;
 
-    @GetMapping
+    @GetMapping(name = "search")
     public List<PedidoDTO> getWithCriteria(PedidoSearchInputDTO pedidoSearchInputDTO) {
         return service.getPedidosWithCriteria(pedidoSearchInputDTO);
     }
 
-    @PostMapping(name = "newPedido/{id_cliente}")
-    public PedidoDTO create(@RequestParam Long id_cliente) {
-        return service.createPedido(id_cliente);
+    @PostMapping(name = "addToCart")
+    public HttpStatus addItem(@RequestParam Long idCliente, @RequestParam Long idMedicamento, @RequestParam int qtd) {
+        return service.addItemToCart(idCliente, idMedicamento, qtd);
+    }
+
+    @PutMapping(name = "alter")
+    public HttpStatus alterItem(@RequestParam Long idCliente, @RequestParam Long idMedicamento, @RequestParam int qtd) {
+        return service.alterItemQtd(idCliente, idMedicamento, qtd);
+    }
+
+    @DeleteMapping(name = "delete")
+    public HttpStatus deleteItem(@RequestParam Long idCliente, @RequestParam Long idMedicamento) {
+        return service.deleteItemFromCart(idCliente, idMedicamento);
     }
 
 }
