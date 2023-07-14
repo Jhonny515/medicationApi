151e8c4ac91a6715fb388abb82d04738260e52f2
Jhonny515 <jonatascemporcentojesus@gmail.com>
2023-07-14 20:25:25 -0300

    feat<PedidoController>: Atualizado retorno dos métodos e adicionado método para finalizar compra

--- src/main/java/com/jhonny/medicationApi/controllers/PedidoController.java
+++ src/main/java/com/jhonny/medicationApi/controllers/PedidoController.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,7 +18,9 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -33,18 +36,32 @@ public class PedidoController {
     }
 
     @PostMapping("addToCart")
-    public HttpStatus addItem(@RequestParam Long idCliente, @RequestParam Long idMedicamento, @RequestParam int qtd) {
-        return service.addItemToCart(idCliente, idMedicamento, qtd);
+    @Transactional
+    public ResponseEntity addItem(@RequestParam Long idCliente, @RequestParam Long idMedicamento, UriComponentsBuilder uriBuilder) {
+        Long idPedido = service.addItemToCart(idCliente, idMedicamento);
+        URI uri = uriBuilder.path("/pedidos/search?id={id}").buildAndExpand(idPedido).toUri();
+        return ResponseEntity.created(uri).body(idPedido);
     }
 
     @PutMapping("alter")
-    public HttpStatus alterItem(@RequestParam Long idCliente, @RequestParam Long idMedicamento, @RequestParam int qtd) {
-        return service.alterItemQtd(idCliente, idMedicamento, qtd);
+    @Transactional
+    public ResponseEntity alterItem(@RequestParam Long idCliente, @RequestParam Long idMedicamento, @RequestParam int qtd, UriComponentsBuilder uriBuilder) {
+        Long idPedido = service.alterItemQtd(idCliente, idMedicamento, qtd);
+        URI uri = uriBuilder.path("/pedidos/search?id={id}").buildAndExpand(idPedido).toUri();
+        return ResponseEntity.created(uri).body(idPedido);
     }
 
     @DeleteMapping("delete")
-    public HttpStatus deleteItem(@RequestParam Long idCliente, @RequestParam Long idMedicamento) {
-        return service.deleteItemFromCart(idCliente, idMedicamento);
+    @Transactional
+    public ResponseEntity deleteItem(@RequestParam Long idCliente, @RequestParam Long idMedicamento, UriComponentsBuilder uriBuilder) {
+        Long idPedido = service.deleteItemFromCart(idCliente, idMedicamento);
+        URI uri = uriBuilder.path("/pedidos/search?id={id}").buildAndExpand(idPedido).toUri();
+        return ResponseEntity.created(uri).body(idPedido);
+    }
+
+    @PutMapping("checkout")
+    public ResponseEntity checkout(@RequestParam Long idCliente) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.checkoutPedido(idCliente));
     }
 
 }
